a7bf19d47847a892bec89d78dc66b0e208d8e276
marcos mezzabotta <marcos32m@gmail.com>
2018-05-31 12:43:09 -0300

    add basic libraries and particles logo

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import logo from "./logo.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Argenticator</h1>
+        </header>
+        <Particles params={particlesJson} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+export const particlesJson = {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#3CA9D1"
+      },
+      polygon: {
+        nb_sides: 5
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 80,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 300,
+      color: "#3CA9D1",
+      opacity: 0.4,
+      width: 0
+    },
+    move: {
+      enable: true,
+      speed: 7,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 280,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 800,
+        size: 80,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+};
